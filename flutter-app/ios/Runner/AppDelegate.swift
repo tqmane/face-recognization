4238ae00b11cdc41868810d76e01b861fa9dbee3
tqmane <user@example.com>
2025-12-02 19:32:34 +0900

    Add app icons and launch images, update storyboards and bridging header
    - Added various app icons in different sizes to the Assets.xcassets/AppIcon.appiconset directory.
    - Included launch images in the Assets.xcassets/LaunchImage.imageset directory.
    - Created LaunchScreen.storyboard and Main.storyboard for the app's launch and main screens.
    - Added a bridging header for Swift compatibility.
    - Implemented SceneDelegate for managing app lifecycle events.
    - Added initial test case in RunnerTests.swift for future testing.

---
+++ flutter-app/ios/Runner/AppDelegate.swift
@@ -0,0 +1,28 @@
+import UIKit
+import Flutter
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
+
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Firebase初期化
+    FirebaseApp.configure()
+    
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  // Google Sign-In用のURL処理
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    return GIDSignIn.sharedInstance.handle(url)
+  }
+}
